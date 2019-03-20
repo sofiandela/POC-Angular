5decf5b0ac930366e1776b51378ff5ca03667e10
Sofi <sofia.suvercase@globallogic.com>
2019-03-20 15:25:33 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'conFede';
+}
